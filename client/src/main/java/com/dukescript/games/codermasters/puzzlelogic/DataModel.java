d66419674736769402fb08f6267a1fff5a1fa1b5
Ruslan López <scherzo_16@hotmail.com>
2016-01-01 19:09:23 -0600

    Se migra totalmente a Dukescript, se cambió la navegación completa.

--- client/src/main/java/com/dukescript/games/codermasters/puzzlelogic/DataModel.java
+++ client/src/main/java/com/dukescript/games/codermasters/puzzlelogic/DataModel.java
@@ -1,8 +1,10 @@
 package com.dukescript.games.codermasters.puzzlelogic;
 
 //import net.java.html.json.ComputedProperty;
+import com.dukescript.games.codermasters.puzzlelogic.js.Dialogs;
 import net.java.html.json.Function;
 import net.java.html.json.Model;
+import net.java.html.json.Models;
 import net.java.html.json.Property;
 //import com.dukescript.games.codermasters.puzzlelogic.js.Dialogs;
 
@@ -15,15 +17,14 @@ import net.java.html.json.Property;
     @Property(name = "contador_segundos", type = int.class),
     @Property(name = "contador_minutos", type = int.class),
     @Property(name = "esta_corriendo_el_tiempo", type = boolean.class),
+    @Property(name = "page", type = String.class),
     @Property(name = "arreglo_numeros", type = int.class, array = true)
 })
 final class DataModel {
-
     /*@ComputedProperty
     static boolean verificarGanarJuego() {
         return false;
     }*/
-
     /**
      * Devuelve los contadores a cero.
      */
@@ -39,7 +40,9 @@ final class DataModel {
      * Called when the page is ready.
      */
     static void onPageLoad() throws Exception {
-        ui = new CntrlDashboard(0, 0, 0, false, null);
+        ui = new CntrlDashboard(0, 0, 0, false, "home", null);
+        Models.toRaw(ui);
+        Dialogs.registerBinding();
         ui.applyBindings();
         //Dialogs.screenSize();
     }
--- js/src/main/java/com/dukescript/games/codermasters/puzzlelogic/js/Dialogs.java
+++ js/src/main/java/com/dukescript/games/codermasters/puzzlelogic/js/Dialogs.java
@@ -1,40 +1,60 @@
 package com.dukescript.games.codermasters.puzzlelogic.js;
 
 import net.java.html.js.JavaScriptBody;
+import net.java.html.js.JavaScriptResource;
 
-/** Use {@link JavaScriptBody} annotation on methods to
- * directly interact with JavaScript. See
+/**
+ * Use {@link JavaScriptBody} annotation on methods to directly interact with
+ * JavaScript. See
  * http://bits.netbeans.org/html+java/1.2/net/java/html/js/package-summary.html
  * to understand how.
+ * @author javatlacati, monacotoni
  */
+@JavaScriptResource(value = "registerRouter.js")
 public final class Dialogs {
+
     private Dialogs() {
     }
-    
-    /** Shows confirmation dialog to the user.
-     * 
+
+    /**
+     * Shows confirmation dialog to the user.
+     *
      * @param msg the message
      * @param callback called back when the use accepts (can be null)
      */
     @JavaScriptBody(
-        args = { "msg", "callback" }, 
-        javacall = true, 
-        body = "if (confirm(msg)) {\n"
-             + "  callback.@java.lang.Runnable::run()();\n"
-             + "}\n"
+            args = {"msg", "callback"},
+            javacall = true,
+            body = "if (confirm(msg)) {\n"
+            + "  callback.@java.lang.Runnable::run()();\n"
+            + "}\n"
     )
     public static native void confirmByUser(String msg, Runnable callback);
-    
+
+    /**
+     * Adjusts document to screen size.
+     * @return Size of the screen
+     */
     @JavaScriptBody(
-        args = {}, body = 
-        "var w = window,\n" +
-        "    d = document,\n" +
-        "    e = d.documentElement,\n" +
-        "    g = d.getElementsByTagName('body')[0],\n" +
-        "    x = w.innerWidth || e.clientWidth || g.clientWidth,\n" +
-        "    y = w.innerHeight|| e.clientHeight|| g.clientHeight;\n" +
-        "\n" +
-        "return 'Screen size is ' + x + ' times ' + y;\n"
+            args = {}, body
+            = "var w = window,\n"
+            + "    d = document,\n"
+            + "    e = d.documentElement,\n"
+            + "    g = d.getElementsByTagName('body')[0],\n"
+            + "    x = w.innerWidth || e.clientWidth || g.clientWidth,\n"
+            + "    y = w.innerHeight|| e.clientHeight|| g.clientHeight;\n"
+            + "\n"
+            + "return 'Screen size is ' + x + ' times ' + y;\n"
     )
     public static native String screenSize();
+
+    /**
+     * This method registers the bindings.
+     * @return ??
+     */
+    @JavaScriptBody(
+            args = {}, body
+            = ""
+    )
+    public static native String registerBinding();
 }
